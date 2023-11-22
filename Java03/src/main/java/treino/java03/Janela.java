ad69e829461a69b11dcb1fa87fdec39797ce8d3d
LeoMarotta <sen.aka@hotmail.com>
2023-11-22 16:08:47 -0300

    Adição de exercicios de uso de JFrame e swing

---
+++ Java03/src/main/java/treino/java03/Janela.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package treino.java03;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+
+
+/**
+ *
+ * @author leomarotta
+ */
+
+public class Janela extends JFrame{
+    JLabel label = new JLabel("Nome");
+        private JTextField txtNome=new JTextField(20);
+        JButton botao=new JButton("ok");
+
+        public Janela(){
+            this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            this.iniciar();
+        }
+
+        private void iniciar(){
+            this.setLayout(new FlowLayout());
+            this.txtNome.setText("Coloque seu nome");
+            this.add(this.label);
+            this.add(this.txtNome);
+            this.add(this.botao);
+            botao.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("Caixa de texto: " + txtNome.getText());
+                }
+            });
+            this.setSize(400, 300);
+        }
+}
---
+++ Java03/src/main/java/treino/java03/Java03.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package treino.java03;
+
+/**
+ *
+ * @author leomarotta
+ */
+
+import javax.swing.JFrame;
+
+public class Java03{
+
+    public static void main(String args[]){
+        JFrame janela1=new Janela();
+        janela1.setVisible(true);
+        JFrame janela2=new Janela();
+        janela2.setVisible(true);
+    }
+}
