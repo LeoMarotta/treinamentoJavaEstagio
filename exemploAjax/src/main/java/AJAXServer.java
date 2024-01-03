e78f84388a50c589ddb5548f99c577bea88c118a
LeoMarotta <sen.aka@hotmail.com>
2024-01-03 11:33:16 -0300

    Inicio de atualizações ajax

--- exemploAjax/src/main/java/AJAXServer.java
+++ exemploAjax/src/main/java/AJAXServer.java
@@ -1,10 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
-
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,72 +10,26 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author leomarotta
  */
+
 @WebServlet(urlPatterns = {"/AJAXServer"})
 public class AJAXServer extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) 
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AJAXServer</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AJAXServer at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String conteudo = req.getParameter("conteudo");
+        if (conteudo == null) {
+            conteudo = "parametro não recebido";
         }
+        Logger.getLogger(AJAXServer.class.getName()).info(String.format("Parametro [conteudo:%s]", conteudo));
+        resp.setContentType("text/xml");
+        resp.setHeader("Cache-Control", "no-cache");
+        resp.getWriter().write(String.format("<retornoajax cor='%s'>%s</retornoajax>", pegaCor(), conteudo.toUpperCase()));
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    
+    private String pegaCor() {
+        String[] cores = {"blue", "yellow", "green", "red", "black","brown","gold","orange","purple","skyblue"};
+        Double pos=Math.random() * 10;
+        return cores[pos.intValue()];
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
