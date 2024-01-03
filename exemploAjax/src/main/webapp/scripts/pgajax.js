66e11d94c5f674b319c322595a588fa207e11af8
LeoMarotta <sen.aka@hotmail.com>
2024-01-03 10:49:54 -0300

    Inicio AJAX

---
+++ exemploAjax/src/main/webapp/scripts/pgajax.js
@@ -0,0 +1,37 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+
+var req;
+var isIE;
+
+function initAJAX() {
+    if (window.XMLHttpRequest) {
+      req = new XMLHttpRequest(); // código para a maioria dos browsers
+    } else if (window.ActiveXObject) {
+        isIE = true;
+        req = new ActiveXObject("Microsoft.XMLHTTP"); // para funcionar no IE
+    }
+}
+
+function avaliaTecla() {
+    initAJAX();
+    req.onreadystatechange = retornoAJAX;
+    var obj = document.getElementById("txtInput");
+    var url = "ajaxserver?conteudo=" + obj.value;
+    req.open("GET", url, true);
+    req.send(null);
+}
+
+function retornoAJAX(){
+        if (req.readyState === 4) {
+        if (req.status === 200) {
+            var elemXML=req.responseXML.documentElement;
+            var objRet = document.getElementById("txtRetorno");
+            objRet.style.backgroundColor=elemXML.getAttribute("cor");
+            objRet.innerHTML=elemXML.childNodes[0].nodeValue;
+        }
+    }
+}
