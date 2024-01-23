654caa139ea4232acb3b90f3b638b3344c207b22
LeoMarotta <sen.aka@hotmail.com>
2024-01-23 10:03:20 -0300

    AppQueryJPA -fix popula

--- AppQueryJPA/AppQueryJPA-ejb/src/java/br/tche/ucpel/tads/doo2/ejb/JPAQueries.java
+++ AppQueryJPA/AppQueryJPA-ejb/src/java/br/tche/ucpel/tads/doo2/ejb/JPAQueries.java
@@ -2,7 +2,9 @@ package br.tche.ucpel.tads.doo2.ejb;
 
 import br.tche.ucpel.tads.doo2.persisty.Departamento;
 import br.tche.ucpel.tads.doo2.persisty.Funcionario;
+import br.tche.ucpel.tads.doo2.persisty.Disciplina;
 import br.tche.ucpel.tads.doo2.persisty.Pessoa;
+import br.tche.ucpel.tads.doo2.persisty.Aluno;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -49,6 +51,56 @@ public class JPAQueries implements JPAQueriesRemote {
             func.setSalario(3000.0);
             func.setCpf("45445");
             em.persist(func);
+
+            //Inserts chatgpt
+            Departamento deptTI = new Departamento();
+            deptTI.setDescricao("TI");
+            em.persist(deptTI);
+
+            Departamento deptVendas = new Departamento();
+            deptVendas.setDescricao("Vendas");
+            em.persist(deptVendas);
+
+            Funcionario func2 = new Funcionario();
+            func2.setDepartamento(deptTI);
+            func2.setDtContratacao(new Date());
+            func2.setDtNascimento(sdf.parse("15/03/1990"));
+            func2.setNome("Ana");
+            func2.setSalario(2500.0);
+            func2.setCpf("78901");
+            em.persist(func2);
+
+            Funcionario func3 = new Funcionario();
+            func3.setDepartamento(deptVendas);
+            func3.setDtContratacao(new Date());
+            func3.setDtNascimento(sdf.parse("10/08/1985"));
+            func3.setNome("Carlos");
+            func3.setSalario(2000.0);
+            func3.setCpf("45678");
+            em.persist(func3);
+
+            Disciplina disciplina2 = new Disciplina();
+            disciplina2.setDescricao("Estatística");
+            disciplina2.setCargaHoraria(60);
+            em.persist(disciplina2);
+
+            Disciplina disciplina3 = new Disciplina();
+            disciplina3.setDescricao("Arquitetura de Software");
+            disciplina3.setCargaHoraria(80);
+            em.persist(disciplina3);
+
+            Aluno aluno2 = new Aluno();
+            aluno2.setNome("Laura");
+            aluno2.setDtNascimento(sdf.parse("05/12/1995"));
+            aluno2.setCpf("12345");
+            aluno2.setSexo(Pessoa.Sexo.FEMININO);
+            em.persist(aluno2);
+
+            aluno2.addDisciplina(disciplina2);
+            aluno2.addDisciplina(disciplina3);
+
+            em.persist(aluno2);
+
         } catch (ParseException ex) {
             Logger.getLogger(JPAQueries.class.getName()).log(Level.SEVERE, "Provavelmente Data Inválida", ex);
         }
