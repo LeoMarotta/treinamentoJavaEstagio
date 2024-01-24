912685b58a5c755df8fd779cca6c383244072c03
LeoMarotta <sen.aka@hotmail.com>
2024-01-24 10:41:29 -0300

    AppJSF -regiao

---
+++ AppJSF/AppJSF-war/src/java/br/edu/ucpel/lp2/jsf/mng/RegiaoMNG.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ucpel.lp2.jsf.mng;
+
+/**
+ *
+ * @author leomarotta
+ */
+public class RegiaoMNG {
+    
+}
